ebf541f093c0dc770f27656d37a8e2253e9b2ae6
siat.testing <siat.testing@gmail.com>
2023-09-08 12:18:00 +0000

    adding svelte and nanostores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // see PocketBase Javascript sdk
+  output: 'server', // see PocketBase Javascript sdk
+  integrations: [svelte()],
 });
---
+++ src/components/Navigation.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { isLoggedIn } from '../lib/stores/users.ts'
+
+</script>
+<nav class="nav-links ">
+    <a href="/">Home</a>
+    <a href="/shops">Shops</a>
+    <a href="/about">About</a>
+    <a href="/auth/register">Register</a>
+    <a href="/auth/login">Login</a>
+    <a href="/auth/logout">Logout</a>
+    {#if $isLoggedIn}
+    <button>You are logged in</button>
+    {/if}
+    
+</nav>
+
+<style>
+    .nav-links {
+        width: 100%;
+        display: flex;
+        background-color: #333;
+    }
+    .nav-links  a {
+        color: white;
+        padding: 14px 20px;
+        text-align: center;
+        text-decoration: none;
+
+
+    }
+    .nav-links  a:hover, .nav-links  a:focus {
+        background-color: #ddd;
+        color: black;
+    }
+</style>
