b300927b4892bf9af18e25f29b21797e7fb1fc1d
siat.testing <siat.testing@gmail.com>
2023-09-13 13:37:53 +0000

    removed vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server', // see PocketBase Javascript sdk
-  adapter: vercel(),
   integrations: [svelte()],
 });
