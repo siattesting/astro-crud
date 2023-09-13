c6724381bac19a5ce98482dfa851095bbfcb1c34
siat.testing <siat.testing@gmail.com>
2023-09-13 13:46:10 +0000

    added netlify server adpater

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server', // see PocketBase Javascript sdk
+  adapter: netlify(),
   integrations: [svelte()],
 });
