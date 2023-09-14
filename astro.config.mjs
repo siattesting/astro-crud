c0a31f2256bebfe6d5a40d9e339a7059bf11dfb6
siat.testing <siat.testing@gmail.com>
2023-09-14 16:06:24 +0000

    removed tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
 import svelte from '@astrojs/svelte';
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   // see PocketBase Javascript sdk
   adapter: netlify(),
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte()],
 });
